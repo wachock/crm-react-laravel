b2d8fbd34592696e2a7bbd96ed9ac53f25f78a7a
Sohrab <website.design1008@gmail.com>
2023-02-10 17:38:21 +0530

    Calendar update

---
+++ resources/js/Admin/Components/TeamAvailability/TeamAvailability.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from '@fullcalendar/timegrid';
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid'
+
+export default function TeamAvailability() {
+    
+  return (
+    <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, resourceTimeGridPlugin ]}
+        initialView = 'resourceTimeGridDay'
+        resources =  {[
+            { id: 'a', title: 'Sohrab' },
+            { id: 'b', title: 'Hasan'},
+            { id: 'c', title: 'Danish' },
+            { id: 'd', title: 'Fateh' },
+            { id: 'e', title: 'Kulwinder' },
+            { id: 'f', title: 'Prashant' },
+            { id: 'g', title: 'Ganesh' },
+            { id: 'H', title: 'Krishna' },
+            { id: 'I', title: 'Avinash' }
+        ]}
+        
+        events={'https://fullcalendar.io/api/demo-feeds/events.json?with-resources=4&single-day'}
+    />
+  )
+}
--- resources/js/Admin/Layouts/Header.js
+++ resources/js/Admin/Layouts/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { useNavigate,Link} from "react-router-dom";
+import { useNavigate} from "react-router-dom";
+import { Link} from "react-router-link";
 import User from '../../Assets/image/user.png';
 import { useAlert } from "react-alert";
 
