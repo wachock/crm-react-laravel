5ea93a5a7c606bf725a07588a6d9d67e2018eb5d
kulwinder <skulwinder@n2rtechnologies.in>
2023-02-16 21:16:58 +0530

    update job

--- resources/js/Admin/Components/TeamAvailability/TeamAvailability.js
+++ resources/js/Admin/Components/TeamAvailability/TeamAvailability.js
@@ -65,15 +65,58 @@ export default function TeamAvailability() {
           start:moment(wa.date+' '+wa.start_time).toISOString(),
           end:moment(wa.date+' '+wa.end_time).toISOString(),
           resourceId: wa.worker_id,
+          textColor: wa.worker_id+'_'+wa.name,
           backgroundColor: '#00FF00',
           borderColor: '#00FF00',
         }; 
     });  
      Array.prototype.push.apply(events,events1);
-     const dateClickHandle = (info) => {
-      alert('clicked ' + info.dateStr + ' on resource ' + info.resource.id);
+
+    const contracts = {
+        client_id :1,
+        client_name:'Kulwinder',
+        services:'Office Cleaning,Regular Room Service',
+        frequency:'once a week',
+        hours:2.5,
+
     }
+     
+     const [data,setData]=useState({client_id:contracts.client_id,
+                                    client_name:contracts.client_name,
+                                    services:contracts.services,
+                                    frequency:contracts.frequency,
+                                    hours:contracts.hours,
+                                     worker_id:'',
+                                     date:'',
+                                     start_time:'',
+                                     end_time:'',
+                                     status:'not-started',
+                                 });
+     const handleEventClick = (e) =>{
+           let str=e.startStr;
+            var parts = str.slice(0, -9).split('T');
+            var dateComponent = parts[0];
+            var timeComponent = parts[1];
+            var endtime = moment(parts[0]+' '+parts[1]).add(contracts.hours, 'hours').format('HH:mm');
+
+            const str1 = e.textColor;
+            var parts1 = str1.split('_');
+
+            var newdata = data;
+            newdata.worker_id=parts1[0];
+            newdata.date=dateComponent;
+            newdata.start_time=timeComponent;
+            newdata.end_time=endtime;
+            document.getElementById('d_worker_name').innerHTML=parts1[1];
+            document.getElementById('d_date').innerHTML=dateComponent;
+            document.getElementById('d_start_time').innerHTML=timeComponent;
+            document.getElementById('d_end_time').innerHTML=endtime;
+            setData(newdata);
+     }
+    console.log(data);
+
   return (
+    <>
     <FullCalendar
         selectable= {true}
         plugins={[dayGridPlugin, timeGridPlugin,resourceTimelinePlugin,interactionPlugin  ]}
@@ -87,9 +130,129 @@ export default function TeamAvailability() {
         slotMaxTime={'24:00'}
         allDaySlot= {false}
         eventClick= {function(info) {
-                    alert('Event: ' + info.event.start);
+                    // alert('Event: ' + info.event.textColor);
+                    // console.log(info.event);
+                    handleEventClick(info.event);
                     info.el.style.borderColor = 'red';
                   } }
-    />       
+    />  
+    <div className="form-group text-center">
+        <input type='button' value='View Job' className="btn btn-pink" data-toggle="modal" data-target="#exampleModal"/>
+    </div> 
+     <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">View Job</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+
+                            <div className="row">
+                                <div className="col-sm-3">
+                                    <div className="form-group">
+                                        <label className="control-label">
+                                            client Name
+                                            <p> {data.client_name}</p>
+                                        </label>
+                                       
+
+                                    </div>
+                                </div>
+                                <div className="col-sm-3">
+                                    <div className="form-group">
+                                        <label className="control-label">
+                                              Services
+                                            <p> {data.services}</p>
+                                        </label>
+                                       
+
+                                    </div>
+                                </div>
+                                <div className="col-sm-3">
+                                    <div className="form-group">
+                                        <label className="control-label">
+                                              Frequency
+                                            <p> {data.frequency}</p>
+                                        </label>
+                                       
+
+                                    </div>
+                                </div>
+                                <div className="col-sm-3">
+                                    <div className="form-group">
+                                        <label className="control-label">
+                                              Hours
+                                            <p> {data.hours} hours</p>
+                                        </label>
+                                       
+
+                                    </div>
+                                </div>
+                                 <div className="col-sm-3">
+                                    <div className="form-group">
+                                        <label className="control-label">
+                                              Worker Name
+                                            <p id="d_worker_name"> {data.worker_id}</p>
+                                        </label>
+                                       
+
+                                    </div>
+                                </div>
+                                <div className="col-sm-3">
+                                    <div className="form-group">
+                                        <label className="control-label">
+                                              Date
+                                            <p id="d_date"> {data.date}</p>
+                                        </label>
+                                       
+
+                                    </div>
+                                </div>
+                                <div className="col-sm-3">
+                                    <div className="form-group">
+                                        <label className="control-label">
+                                              Start Time
+                                            <p id="d_start_time"> {data.start_time}</p>
+                                        </label>
+                                       
+
+                                    </div>
+                                </div>
+                                <div className="col-sm-3">
+                                    <div className="form-group">
+                                        <label className="control-label">
+                                              End Time
+                                            <p id="d_end_time"> {data.end_time}</p>
+                                        </label>
+                                       
+
+                                    </div>
+                                </div>
+                                <div className="col-sm-3">
+                                    <div className="form-group">
+                                        <label className="control-label">
+                                              Status
+                                            <p > {data.status}</p>
+                                        </label>
+                                       
+
+                                    </div>
+                                </div>
+                                
+                            </div>
+
+
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary closeb" data-dismiss="modal">Close</button>
+                            <button type="button"  className="btn btn-primary">Save and Send</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </>    
   )
 }
--- resources/js/Admin/Pages/Jobs/AddJob.js
+++ resources/js/Admin/Pages/Jobs/AddJob.js
@@ -11,145 +11,6 @@ import TeamAvailability from '../../Components/TeamAvailability/TeamAvailability
 export default function AddJob() {
     const alert                        = useAlert();
     const navigate                     = useNavigate();
-    const [service, setService]        = useState('');
-    const [client, setClient]          = useState('');
-    const [worker, setWorker]          = useState('');
-    const [startDate, setStartDate]    = useState('');
-    const [endDate, setEndDate]        = useState('');
-    const [startTime, setStartTime]    = useState('');
-    const [endTime, setEndTime]        = useState('');
-    const [rate, setRate]              = useState('');
-    const [instruction,setInstruction] = useState('');
-    const [address, setAddress]        = useState('');
-    const [status, setStatus]          = useState('');
-    const [schedule, setSchedule]      = useState('');
-
-    const [AllClients,setAllClients]   = useState([]);
-    const [AllServices,setAllServices] = useState([]);
-    const [AllWorkers,setAllWorkers]   = useState([]);
-    const [AllSchedules,setAllSchedules]   = useState([]);
-    
-    
-    const headers = {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ` + localStorage.getItem("admin-token"),
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const data = {
-            client_id:client,
-            worker_id:worker,
-            start_date:startDate,
-            start_time:startTime,
-            end_time:endTime,
-            schedule:schedule,
-            status:status
-        };
-        axios
-        .post(`/api/admin/jobs`,data,{headers})
-        .then((res)=>{
-            if(res.data.error){
-                for (let e in res.data.error){
-                    alert.error(res.data.error[e]);
-                }
-                } else {
-                alert.success(res.data.message);
-                setTimeout(()=>{
-                    navigate('/admin/jobs');
-                },1000);
-                
-                }
-        });
-    }
-
-    const getClients = () =>{
-       axios
-       .get('/api/admin/all-clients',{headers})
-       .then((res)=>{
-         setAllClients(res.data.clients);
-       })
-       
-    }
-
-    const getServices = () =>{
-        axios
-        .get('/api/admin/all-services',{headers})
-        .then((res)=>{
-          setAllServices(res.data.services);
-        })
-     }
-
-     const getWorkers = () =>{
-        axios
-        .get('/api/admin/all-workers',{headers})
-        .then((res)=>{
-          setAllWorkers(res.data.workers);
-        })
-     }
-    
-     const getSchedule = () =>{
-        axios
-        .get('/api/admin/all-service-schedule',{headers})
-        .then((res)=>{
-            setAllSchedules(res.data.schedules);
-        })
-     }
-    useEffect(()=>{
-        getClients();
-        getServices();
-        getWorkers();
-        getSchedule();
-    },[]);
-    
-    const cData = AllClients.map((c,i)=>{
-       return {value:c.id,label:(c.firstname+' '+c.lastname)}; 
-    });
-    const sData = AllServices.map((s,i)=>{
-        return {value:s.id,label:(s.name)}; 
-    });
-    const wData = AllWorkers.map((w,i)=>{
-        return {value:w.id,label:(w.firstname+' '+w.lastname)}; 
-    });
-    const scData = AllSchedules.map((sc,i)=>{
-        return {value:sc.id,label:sc.name}; 
-    });
-    const time =[
-                ["08:00","08:00 AM"],
-                ["08:30","08:30 AM"],
-                ["09:00","09:00 AM"],
-                ["09:30","09:30 AM"],
-                ["10:00","10:00 AM"],
-                ["10:30","10:30 AM"],
-                ["11:00","11:00 AM"],
-                ["11:30","11:30 AM"],
-                ["12:00","12:00 PM"],
-                ["12:30","12:30 PM"],
-                ["13:00","01:00 PM"],
-                ["13:30","01:30 PM"],
-                ["14:00","02:00 PM"],
-                ["14:30","02:30 PM"],
-                ["15:00","03:00 PM"],
-                ["15:30","03:30 PM"],
-                ["16:00","04:00 PM"],
-                ["16:30","04:30 PM"],
-                ["17:00","05:00 PM"],
-                ["17:30","05:30 PM"],
-                ["18:00","06:00 PM"],
-                ["18:30","06:30 PM"],
-                ["19:00","07:00 PM"],
-                ["19:30","07:30 PM"],
-                ["20:00","08:00 PM"],
-                ["20:30","08:30 PM"],
-                ["21:00","09:00 PM"],
-                ["21:30","09:30 PM"],
-                ["22:00","10:00 PM"],
-                ["22:30","10:30 PM"],
-                ["23:00","11:00 PM"],
-                ["23:30","11:30 PM"],
-                ["00:00","12:00 AM"],
-            ];
     
   return (
     <div id="container">
@@ -160,19 +21,37 @@ export default function AddJob() {
                 <div id='calendar'></div>
                 <div className='card'>
                     <div className='card-body'>
-                        <form onSubmit={handleSubmit}>     
+                        <form>     
                             <div className='row'>
-                                <div className='col-sm-6'>
-                                    <div className="form-group">
-                                        <label className="control-label">Client Name</label>
-                                        <SelectPicker data={cData} value={client} onChange={(value,event)=>setClient(value)} size="lg" required/>
-                                    </div>
+                                <div className='col-sm-2'>
+                                          <div className='form-group'>
+                                            <label>Client</label>
+                                            <p>Kulwinder</p>
+                                        </div>
                                 </div>
-                                <div className='col-sm-6'>
-                                    <div className="form-group">
-                                        <label className="control-label">Worker Name</label>
-                                        <SelectPicker data={wData} value={worker} onChange={(value,event)=>setWorker(value)} size="lg" required/>
-                                    </div>
+                                 <div className='col-sm-2'>
+                                          <div className='form-group'>
+                                            <label>Services</label>
+                                            <p>Office Cleaning,Regular Room Service</p>
+                                        </div>
+                                </div>
+                                <div className='col-sm-2'>
+                                          <div className='form-group'>
+                                            <label>Frequency</label>
+                                            <p>Once a week</p>
+                                        </div>
+                                </div>
+                                 <div className='col-sm-2'>
+                                          <div className='form-group'>
+                                            <label>Complete Time</label>
+                                            <p>2.5 hour</p>
+                                        </div>
+                                </div>
+                                 <div className='col-sm-4'>
+                                          <div className='form-group'>
+                                            <label>Address</label>
+                                            <p>Delhi</p>
+                                        </div>
                                 </div>
                                 <div className='col-sm-12'>
                                     <div className='mt-3 mb-3'>
@@ -183,50 +62,8 @@ export default function AddJob() {
                                     <TeamAvailability/>
                                     <div className='mb-3'>&nbsp;</div>
                                 </div>
-                                <div className='col-sm-3'>
-                                    <div className="form-group">
-                                        <label className="control-label">Start Date</label>
-                                        <input type="date" value={startDate} required onChange={(e) => setStartDate(e.target.value)} className='form-control' />
-                                    </div>
-                                </div>
-                                <div className='col-sm-3'>
-                                     <div className='form-group'>
-                                    <label>Start Time</label>
-                                    <select name="start_time" id="start_time"  onChange={(e)=>{setStartTime(e.target.value)}} className="form-control">
-                                        <option>Choose start time</option>
-                                        {time && time.map((t,i)=>{
-                                            return (<option value={t[0]} selected={t[0] == startTime}>{t[1]}</option>);
-                                        })}
-                                       
-                                    </select>
-                                </div>
-                                </div>
-                                <div className='col-sm-3'>
-                                    <div className='form-group'>
-                                    <label>End Time</label>
-                                    <select name="end_time" id="end_time" selected={endTime} onChange={(e)=>{setEndTime(e.target.value)}} className="form-control">
-                                        <option>Choose end time</option>
-                                        {time && time.map((t,i)=>{
-                                            return (<option value={t[0]} selected={t[0] == endTime}>{t[1]}</option>);
-                                        })}
-                                    </select>
-                                </div>
-                                </div>
-                                <div className='col-sm-3'>
-                                    <div className="form-group">
-                                        <label className="control-label">Status</label>
-                                        <select className="form-control" value={status} onChange={(e) => setStatus(e.target.value)}>
-                                            <option>Please Select</option>
-                                            <option value="not-started">Not Started</option>
-                                            <option value="progress">Progress</option>
-                                            <option value="completed">Completed</option>
-                                        </select>
-                                    </div>
-                                </div>
                             </div>
-                            <div className="form-group text-center">
-                                <input type='submit' value='Save and Send' onClick={handleSubmit} className="btn btn-pink saveBtn"/>
-                            </div>
+                            
                         </form>
                     </div>
                 </div>
