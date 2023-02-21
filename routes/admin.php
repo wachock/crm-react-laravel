5442ee67060299bc3707accf4c0f6ff2bec42868
kulwinder <skulwinder@n2rtechnologies.in>
2023-02-21 15:20:12 +0530

    update cron

--- routes/admin.php
+++ routes/admin.php
@@ -43,6 +43,8 @@ Route::group(['prefix' => 'admin'], function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
     Route::get('weeklyjob', [CronController::class, 'WeeklyJob']);
+    Route::get('check_worker', [CronController::class, 'checkWorker']);
+
 
 });
 
