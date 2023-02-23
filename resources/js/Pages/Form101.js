78cba8bb0207418b6e1b3126388c4f7bb51d2d1f
Sohrab <website.design1008@gmail.com>
2023-02-23 11:53:41 +0530

    101 form added

---
+++ resources/js/Pages/Form101.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Form101() {
+  return (
+    <div className='container'>
+      <div className='form101'>
+        <iframe width="100%" id="tofes-tepez-iframe" src="https://app.tepez.co.il/f/5a704edf239da27488a8cd9c/run"></iframe>
+      </div>
+    </div>
+  )
+}
