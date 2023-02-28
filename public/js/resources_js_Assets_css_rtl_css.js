4405d03f5ba6b55d6601891ec1781f261dfe8633
Sohrab <website.design1008@gmail.com>
2023-02-28 16:23:07 +0530

    Create resources_js_Assets_css_rtl_css.js

---
+++ public/js/resources_js_Assets_css_rtl_css.js
@@ -0,0 +1,57 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_Assets_css_rtl_css"],{
+
+/***/ "./node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[6].oneOf[1].use[1]!./node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[6].oneOf[1].use[2]!./resources/js/Assets/css/rtl.css":
+/*!***************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[6].oneOf[1].use[1]!./node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[6].oneOf[1].use[2]!./resources/js/Assets/css/rtl.css ***!
+  \***************************************************************************************************************************************************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__);
+// Imports
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
+// Module
+
+___CSS_LOADER_EXPORT___.push([module.id, ".client-contract .info-list .icons{\n    margin-right: 0;\n    margin-left: 100px;\n    direction: rtl;\n}\n.client-contract .info-list .icons h4{\n    text-align: right;\n    direction: rtl;\n}\n.send-offer p{\n    text-align: right;\n    direction: rtl;\n}\n.client-contract .shift-30 .agg-list .icons {\n    margin-left: 10px;\n    margin-right: 0;\n    direction: rtl;\n}\n.client-contract .shift-30 h6{\n    text-align: right;\n    direction: rtl;\n}\n.worker-contract .shift-30 h5{\n    text-align: right;\n    direction: rtl;\n}\n.worker-contract .sigCanvas{\n    float: right;\n    direction: rtl;\n}\n.worker-contract .btn-warning{\n    float: right;\n    direction: rtl;\n    margin-top: 10px;\n}\n.sendOfferRtl .float-right{\n    float: none !important;\n    direction: rtl;\n}\n.sendOfferRtl h1{\n    text-align: right;\n    direction: rtl;\n}\n.sendOfferRtl .abt h2{\n    text-align: right;\n    direction: rtl;\n}\n.sendOfferRtl .we-have h3{\n    text-align: right;\n    direction: rtl;\n}\n.sendOfferRtl .shift-20{\n    text-align: right;\n    direction: rtl;\n}\n.sendOfferRtl .shift-20 ul li img{\n    direction: rtl;\n    margin-left: 10px;\n}\n.send-offer .abt h2, .send-offer h3, .client-contract h2, .client-contract h3{\n    text-align: right;\n    direction: rtl;\n}\n.client-contract .signed p, .client-contract h4.inHead{\n    text-align: right;\n    direction: rtl;\n}\n#column-left{\n    right: 0;\n    direction: rtl;\n    border-right: none;\n    border-left: 1px solid #E6E8EB;\n}\n#column-left + #content, #column-left + #content{\n    padding-left: 30px;\n    padding-right: 285px;\n    direction: rtl;\n}\n.AdminHeader .float-right{\n    float: none !important;\n    direction: rtl;\n}\n.AdminHeader h1{\n    text-align: right;\n    margin-left: 0;\n    margin-right: 270px;\n    direction: rtl;\n}\n.AdminHeader .dropdown {\n    padding-right: 0;\n    padding-left: 20px;\n    direction: rtl;\n}\n.AdminHeader .dropdown-toggle::after{\n    left: -8px;\n    direction: rtl;\n}\n#column-left .list-group-item a{\n    text-align: right;\n    direction: rtl;\n}\n#column-left ul li a i{\n    margin-right: 0;\n    margin-left: 10px;\n    direction: rtl;\n}\n.adminDash .dashBox .dashIcon{\n    margin-right: 0 !important;\n    margin-left: 1.5rem !important;\n    direction: rtl;\n}\n.adminDash .dashBox .dashText, .boxPanel table tr th, .boxPanel table tr td, .worker-profile h2{\n    text-align: right;\n    direction: rtl;\n}\n.page-title{\n    text-align: right;\n    direction: rtl;\n}\n.search-data{\n    float: left;\n    display: unset;\n    direction: rtl;\n}\n.ViewOffer{\n    padding: 0;\n    text-align: right;\n    direction: rtl;\n}\n.client-view-job .form-group, .noteby, .rtl-comment{\n    text-align: right;\n    direction: rtl;\n}\n.settings-page .nav-tabs{\n    padding-right: 0;\n    direction: rtl;\n}\n.settings-page .form-group{\n    text-align: right;\n    direction: rtl;\n}\n@media (max-width: 767px){\n.mobileNav .navbar-collapse.show, .mobileNav .navbar-toggler.collapsed ~ .navbar-collapse{\n    left: unset;\n    right: 0;\n    direction: rtl;\n    transition: right 100ms ease-in-out;\n}\n.mobileNav .nav-item a{\n    text-align: right;\n    direction: rtl;\n}\n.navbar-nav{\n    padding-right: 0 !important;\n    direction: rtl;\n}\n.mobileNav ul li a i{\n    margin-right: 0;\n    margin-left: 10px;\n    direction: rtl;\n}\n#column-left + #content, #column-left + #content{\n    padding-right: 30px;\n    direction: rtl;\n}\n.responsiveTable td.pivoted{\n    padding-left: 10px !important;\n    direction: rtl;\n}\n.responsiveTable td .tdBefore{\n    text-align: right !important;\n    width: calc(100% - 20px) !important;\n    direction: rtl;\n}\n.responsiveTable tbody tr td:last-child{\n    text-align: left !important;\n    direction: rtl;\n}\n.responsiveTable td.pivoted .d-flex{\n    display: unset !important;\n    position: relative;\n    left: -5px;\n    direction: rtl;\n}\n.navbar-dark .navbar-brand{\n    margin-right: 0 !important;\n    direction: rtl;\n}\n.search-data{\n    float: right;\n    direction: rtl;\n}\n.search-data input{\n    margin-right: 0 !important;\n    direction: rtl;\n}\n.client-contract h4.inHead{\n    font-size: 14px;\n    line-height: 20px;\n    margin-top: 10px;\n    direction: rtl;\n}\n.client-contract .signed p{\n    font-size: 14px;\n    direction: rtl;\n}\n.client-contract .first ul {\n    text-align: right;\n    padding-right: 0;\n    direction: rtl;\n}\n}\n", ""]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./resources/js/Assets/css/rtl.css":
+/*!*****************************************!*\
+  !*** ./resources/js/Assets/css/rtl.css ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../../../../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_ruleSet_1_rules_6_oneOf_1_use_1_node_modules_postcss_loader_dist_cjs_js_ruleSet_1_rules_6_oneOf_1_use_2_rtl_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../../../../node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[6].oneOf[1].use[1]!../../../../node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[6].oneOf[1].use[2]!./rtl.css */ "./node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[6].oneOf[1].use[1]!./node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[6].oneOf[1].use[2]!./resources/js/Assets/css/rtl.css");
+
+            
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_ruleSet_1_rules_6_oneOf_1_use_1_node_modules_postcss_loader_dist_cjs_js_ruleSet_1_rules_6_oneOf_1_use_2_rtl_css__WEBPACK_IMPORTED_MODULE_1__["default"], options);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_ruleSet_1_rules_6_oneOf_1_use_1_node_modules_postcss_loader_dist_cjs_js_ruleSet_1_rules_6_oneOf_1_use_2_rtl_css__WEBPACK_IMPORTED_MODULE_1__["default"].locals || {});
+
+/***/ })
+
+}]);
