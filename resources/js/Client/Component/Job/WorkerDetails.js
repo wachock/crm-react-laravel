2a60d728e903cb90ccdd7f0cdaf89b82a42ec46f
fateh <gfateh@n2rtechnologies.in>
2023-02-28 18:51:58 +0530

    update offer + client work

--- resources/js/Client/Component/Job/WorkerDetails.js
+++ resources/js/Client/Component/Job/WorkerDetails.js
@@ -1,6 +1,8 @@
 import React  from 'react'
+import { useTranslation } from 'react-i18next'
 
 export default function WorkerDetails({worker}) {
+    const {t} = useTranslation();
   return (
     <>
                     <div className='dashBox p-4 mb-3 mt-3'>
@@ -8,25 +10,25 @@ export default function WorkerDetails({worker}) {
                             <div className='row'>
                                 <div className='col-sm-4'>
                                     <div className='form-group'>
-                                        <label className='control-label'>Worker Name</label>
+                                        <label className='control-label'>{t('client.jobs.view.w_name')}</label>
                                          <p>{worker.firstname} {worker.lastname}</p>
                                     </div>
                                 </div>
                                 <div className='col-sm-4'>
                                     <div className='form-group'>
-                                        <label className='control-label'>Worker Email</label>
+                                        <label className='control-label'>{t('client.jobs.view.w_email')}</label>
                                           <p>{worker.email}</p>
                                     </div>
                                 </div>
                                 <div className='col-sm-4'>
                                     <div className='form-group'>
-                                        <label className='control-label'>Worker Phone</label>
+                                        <label className='control-label'>{t('client.jobs.view.w_phone')}</label>
                                        <p>{worker.phone}</p>
                                     </div>
                                 </div>
                                 <div className='col-sm-8'>
                                     <div className='form-group'>
-                                        <label className='control-label'>Address</label>
+                                        <label className='control-label'>{t('client.jobs.view.address')}</label>
                                         <p>{worker.address}</p>
                                     </div>
                                 </div>
