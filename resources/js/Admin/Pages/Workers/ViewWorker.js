71925f5d1debf5258be953f1f328d83b0b9bd1bb
kulwinder <skulwinder@n2rtechnologies.in>
2023-03-09 19:39:50 +0530

    shift validation

--- resources/js/Admin/Pages/Workers/ViewWorker.js
+++ resources/js/Admin/Pages/Workers/ViewWorker.js
@@ -2,14 +2,28 @@ import React from 'react'
 import WorkerHistory from '../../Components/Workers/WorkerHistory'
 import WorkerProfile from '../../Components/Workers/WorkerProfile'
 import Sidebar from '../../Layouts/Sidebar'
+import { Link,useParams } from "react-router-dom";
 
 export default function ViewWorker() {
+    const param = useParams();
   return (
     <div id='container'>
         <Sidebar/>
         <div id="content">
+             <div className="titleBox customer-title">
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <h1 className="page-title">View Worker</h1>
+                        </div>
+                        <div className="col-sm-6">
+                            <div className="search-data">
+                               
+                                <Link  to={`/admin/edit-worker/${param.id}`} className="btn btn-pink addButton"><i className="btn-icon fas fa-pencil"></i>Edit</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             <div className='view-applicant'>
-                <h1 className="page-title viewAppli">View Worker</h1>
                 <WorkerProfile/>
                 <WorkerHistory/>     
             </div>
