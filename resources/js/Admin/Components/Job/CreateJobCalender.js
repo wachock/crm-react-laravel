59e2ab44d2d5db0203062d00609aac3c9fe85427
kulwinder <skulwinder@n2rtechnologies.in>
2023-03-10 19:58:49 +0530

    multi select'

--- resources/js/Admin/Components/Job/CreateJobCalender.js
+++ resources/js/Admin/Components/Job/CreateJobCalender.js
@@ -45,14 +45,6 @@ export default function CreateJobCalender() {
     useEffect(() => {
         getJob();
     }, []);
-    // let time_period;
-    // let service_id;
-    // {
-    //     services && services.map((item, index) => {
-    //         time_period = (item.jobHours);
-    //         service_id = parseInt(item.service);
-    //     })
-    // }
     const getJobs = () => {
         axios
             .get('/api/admin/get-all-jobs', { headers })
@@ -88,104 +80,8 @@ export default function CreateJobCalender() {
         getTime();
     }, []);
     
-    // const resources = AllWorkers.map((w, i) => {
-    //     return { id: w.id, title: (w.firstname + ' ' + w.lastname) };
-    // });
-    // const events = AllJobs.map((j, i) => {
-    //     return {
-    //         id: j.id,
-    //         title: 'Busy',
-    //         start: moment(j.start_date + ' ' + j.start_time).toISOString(),
-    //         end: moment(j.start_date + ' ' + j.end_time).toISOString(),
-    //         resourceId: j.worker_id,
-    //         backgroundColor: '#ff0000',
-    //         borderColor: '#ff0000',
-    //     };
-    // });
-    // const events1 = AllWorkerAvailability.map((wa, i) => {
-    //     return {
-    //         id: wa.id,
-    //         title: 'Available',
-    //         start: moment(wa.date + ' ' + wa.start_time).toISOString(),
-    //         end: moment(wa.date + ' ' + wa.end_time).toISOString(),
-    //         resourceId: wa.worker_id,
-    //         textColor: wa.worker_id + '_' + wa.name,
-    //         backgroundColor: '#00FF00',
-    //         borderColor: '#00FF00',
-    //     };
-    // });
-    // Array.prototype.push.apply(events, events1);
-
-    
-
-    // const handleEventClick = (e,s) => {
-    //     let str = e.startStr;
-    //     var parts = str.slice(0, -9).split('T');
-    //     var dateComponent = parts[0];
-    //     var timeComponent = parts[1];
-    //     var endtime = moment(parts[0] + ' ' + parts[1]).add(time_period, 'hours').format('HH:mm');
-
-    //     const str1 = e.textColor;
-    //     var parts1 = str1.split('_');
-        
-    //      let worker_start = moment(e.startStr).toISOString();
-    //      let worker_end = moment(parts[0] + ' ' + parts[1]).add(time_period, 'hours').toISOString();
-    //       let check_worker_start = true;
-    //       let check_worker_end = true;
-    //       AllWorkerAvailability.map((wa, i) => {
-    //            let new_date= moment(wa.date + ' ' + wa.start_time).toISOString();
-    //            let new_end_date =moment(wa.date + ' ' + wa.end_time).toISOString();
-    //            if(new_date == worker_start){
-    //                 check_worker_start=false;
-    //            }
-    //             if(new_end_date == worker_end){
-    //                 check_worker_end=false;
-    //            }
-    //       });
-        // if(check_worker_start || check_worker_end){
-        //     window.alert('Worker not Available on This Time Period');
-        //     s.style.borderColor = 'rgb(0 255 0)';
-        //     return false;
-        // }
-        // if (data.length > 0) {
-        //     let new_data = [];
-        //     let found = true;
-        //     data.map((d) => {
-        //         if (d.worker_id == parts1[0] && d.date == dateComponent && d.start == timeComponent) {
-        //             found = false;
-        //         } else {
-        //             new_data.push(d)
-        //         }
-
-        //     })
-
-        //     if (found) {
-        //         var newdata = [...data, {
-        //             worker_id: parts1[0],
-        //             worker_name: parts1[1],
-        //             date: dateComponent,
-        //             start: timeComponent,
-        //             end: endtime
-        //         }]
-        //     } else {
-        //         var newdata = new_data;
-        //     }
-
-        // } else {
-    //         var newdata = [...data, {
-    //             worker_id: parts1[0],
-    //             worker_name: parts1[1],
-    //             date: dateComponent,
-    //             start: timeComponent,
-    //             end: endtime
-    //         }]
-    //     }
-    //     setData(newdata);
-    // }
+  
     let service_id;
-    // useEffect(() => {
-    // (services.length>1)?($('#edit-work-time').modal('show')):getWorkers();
-    // }, []);
 
     const handleServices = (value) => {
        const filtered = services.filter((s)=>{
@@ -307,7 +203,7 @@ export default function CreateJobCalender() {
   { value: 1, label: 'morning - 8-12pm' },
 ]
 const changeShift = (w_id,date,e) =>{
-         console.log(e);
+         // console.log(e);
          let w_n = $('#worker-'+w_id).html();
          let  filtered = data.filter((d)=>{
                 if(d.date != date){
@@ -316,16 +212,16 @@ const changeShift = (w_id,date,e) =>{
             });
          let shifts = '';
 
-         // e.map((v)=>{
-         //       if(shifts==''){
-         //         shifts=v.label;
-         //       }else{
-         //         shifts=shifts+','+v.label;
-         //       }
-         // });
-         if(e != null){
-            shifts=e.label;
-         }
+         e.map((v)=>{
+               if(shifts==''){
+                 shifts=v.label;
+               }else{
+                 shifts=shifts+','+v.label;
+               }
+         });
+         // if(e != null){
+         //    shifts=e.label;
+         // }
          var newdata ;
          if(shifts != ''){
               newdata = [...filtered, {
@@ -422,9 +318,10 @@ const filterOptions = (options,shifts) =>{
                                        })}
                                        {(aval[element] && aval[element] != '')?
                                         <Select
+                                            isMulti
                                             name="colors"
                                             options={filterOptions(colourOptions[aval[element]],shifts)}
-                                            className="basic-single"
+                                            className="basic-multi-single"
                                             isClearable={true}
                                             classNamePrefix="select"
                                             onChange={(e)=>changeShift(w.id,element,e)}
@@ -473,9 +370,10 @@ const filterOptions = (options,shifts) =>{
                                        
                                         {(aval[element] && aval[element] != '')?
                                         <Select
+                                            isMulti
                                             name="colors"
                                             options={filterOptions(colourOptions[aval[element]],shifts)}
-                                            className="basic-single"
+                                            className="basic-multi-single"
                                             isClearable={true}
                                             classNamePrefix="select"
                                             onChange={(e)=>changeShift(w.id,element,e)}
@@ -524,9 +422,10 @@ const filterOptions = (options,shifts) =>{
                                        })}
                                          {(aval[element] && aval[element] != '')?
                                         <Select
+                                            isMulti
                                             name="colors"
                                             options={filterOptions(colourOptions[aval[element]],shifts)}
-                                            className="basic-single"
+                                            className="basic-multi-single"
                                             isClearable={true}
                                             classNamePrefix="select"
                                             onChange={(e)=>changeShift(w.id,element,e)}
