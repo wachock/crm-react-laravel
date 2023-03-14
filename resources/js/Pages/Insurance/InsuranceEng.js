423748fdbce6e6fd871dbc14f255858593a0df4b
Avinashe <Avinash>
2023-03-14 11:48:44 +0530

    changes is updated

---
+++ resources/js/Pages/Insurance/InsuranceEng.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function InsuranceEng() {
+  return (
+    <div>
+      <div className='container'>
+        <div className='send-offer insurance-eng'>
+            <div className='maxWidthControl dashBox mb-4'>
+                <h1>Meno Medic Top - Insurance Health for Proposal</h1>
+                <p>All questions must be answered clearly and fully. Do not use lines or symbols instead of words.
+                The form is for both men and women.</p>
+                <p>You must provide full and honest answers to every essential matter you are asked about' and not doing so may have an impact on the payment of insurance benefits</p>
+            </div>
+            </div>
+            </div>
+    </div>
+  )
+}
---
+++ resources/js/Pages/Insurance/InsuranceHeb.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function InsuranceHeb() {
+  return (
+    <div>
+        <div className='container'>
+        <div className='send-offer client-contract sendOfferRtl'>
+            <div className='maxWidthControl dashBox mb-4'>
+                <h1>Hello world</h1>
+            </div>
+            </div>
+            </div>
+    </div>
+  )
+}
