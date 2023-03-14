fd998b2ef1c98d79fe4bc110060a4a830cdf09fa
Avinashe <Avinash>
2023-03-14 20:07:00 +0530

    changes is updated

--- resources/js/Pages/Insurance/InsuranceEng.js
+++ resources/js/Pages/Insurance/InsuranceEng.js
@@ -1,5 +1,6 @@
 import React,{ useRef } from 'react'
-import SignatureCanvas from 'react-signature-canvas'
+import SignatureCanvas from 'react-signature-canvas';
+import logo from "../../Assets/image/logo.png";
 
 export default function InsuranceEng() {
     const sigRef2 = useRef();
@@ -48,6 +49,7 @@ export default function InsuranceEng() {
       <div className='container'>
         <div className='send-offer insurance-eng'>
             <div className='maxWidthControl dashBox mb-4'>
+            <img src={logo} className='img-fluid offer-logo' alt='Broom Service' />
                 <h1>Meno Medic Top - Insurance Health for Proposal</h1>
                 <p>All questions must be answered clearly and fully. Do not use lines or symbols instead of words.
                 The form is for both men and women.</p>
@@ -788,6 +790,9 @@ Mr/Ms___________________________, to handle on my behalf and for me all matters 
 
 
                         </table>
+                    </div>
+                    <div className='button mt-4 text-center'>
+                        <input class="btn btn-pink" value="Save" />
                     </div>
                 </div>
             </div>
