29b91e5ea15402be732a70c37c4b5738a840aca8
kulwinder <skulwinder@n2rtechnologies.in>
2023-03-15 12:38:14 +0530

    update email

--- resources/js/Admin/Pages/Jobs/TotalJobs.js
+++ resources/js/Admin/Pages/Jobs/TotalJobs.js
@@ -372,7 +372,6 @@ export default function TotalJobs() {
                                                                         <i className="fa fa-ellipsis-vertical"></i>
                                                                     </button>
                                                                     <div className="dropdown-menu">
-                                                                        <Link to={`/admin/edit-job/${item.id}`} className="dropdown-item">Edit Job</Link>
                                                                         <Link to={`/admin/view-job/${item.id}`} className="dropdown-item">View</Link>
                                                                         <button className="dropdown-item" onClick={() => handleDelete(item.id)}>Delete</button>
                                                                     </div>
