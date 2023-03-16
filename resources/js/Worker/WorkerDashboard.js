81c622b0ad5f312c0f7d327702d3aa40ec5b2b0e
fateh <gfateh@n2rtechnologies.in>
2023-03-16 14:58:45 +0530

    meeting today first 2

--- resources/js/Worker/WorkerDashboard.js
+++ resources/js/Worker/WorkerDashboard.js
@@ -80,8 +80,7 @@ export default function WorkerDashboard() {
                                             {latestJobs &&
                                                 latestJobs.map(
                                                     (item, index) => {
-                                                       let services =  (item.offer.services) ? JSON.parse(item.offer.services) : [];
-                                                   
+                                                     
                                                     return (
 
                                                         <Tr key={index}>
