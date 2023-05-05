351c24550d36e28b9415101a9d5b4e96f8d392bf
Nurul Hasan <nhasan@n2rtechnologies.com>
2023-05-05 19:04:51 +0530

    Deply

---
+++ deploy.php
@@ -0,0 +1,49 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+require 'contrib/npm.php';
+
+// Config
+
+set('repository', 'git@gitlab.n2rtechnologies.com:nurulhasan/broom-service.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+host('production')
+    ->set('hostname','13.126.49.2' )
+    ->set('branch', 'main')
+    ->set('remote_user', 'root')
+    ->set('deploy_path', '/var/www/html');
+
+host('development')
+    ->set('hostname','38.242.196.238' )
+    ->set('branch', 'main')
+    ->set('remote_user', 'root')
+    ->set('deploy_path', '/var/www/php81/broom-service-new')
+    ->set('keep_releases', 2);
+
+// Hooks
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'artisan:storage:link',
+    'artisan:view:cache',
+    'artisan:config:cache',
+    'artisan:migrate',
+    'npm:install',
+    'npm:run:prod',
+    'deploy:publish',
+]);
+
+task('npm:run:prod', function () {
+    cd('{{release_or_current_path}}');
+    run('composer install');
+    run('npm run prod');
+});
+
+after('deploy:failed', 'deploy:unlock');
